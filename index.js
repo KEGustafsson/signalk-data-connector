04acadc54c05a2a249f7cde1e32e4c86a318f1ad
Karl-Erik Gustafsson <ke.gustafsson@gmail.com>
2023-12-08 15:14:39 +0200

    fix: more error msg covered

--- index.js
+++ index.js
@@ -179,13 +179,14 @@ module.exports = function createPlugin(app) {
         pingTimeout.refresh();
       });
 
-     myMonitor.on("error", function (error) {
-        if (error && error.code === 'ENOTFOUND') {
-          console.error(`Error: Could not resolve the address ${options.testAddress}. Please check the hostname and try again.`);
-        } else {
-          console.error('An unexpected error occurred:', error.message || error);
+      myMonitor.on("error", function (error) {
+        if (error) {
+          const errorMessage = (error.code === 'ENOTFOUND' || error.code === 'EAI_AGAIN') ?
+            `Error: Could not resolve the address ${options.testAddress}. Please check the hostname and try again.` :
+            `An unexpected error occurred: ${error.message || error}`;
+          console.error(errorMessage);
         }
-     });
+      });
 
       pingTimeout = setTimeout(() => {
         readyToSend = false;
